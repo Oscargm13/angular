c613ac9ccbe2fb62eae0c6e7159bbedbc31bdeab
Oscargm13 <oskitargm5@gmail.com>
2024-11-06 21:23:36 +0100

    6/11

--- primerangular/src/app/app.module.ts
+++ primerangular/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HooksAngular } from '../components/hooksangular/hooksangular.component';
 import { DeportesComponent } from '../components/deportes/deportes.component';
+import { FormsbindingComponent } from '../components/formsbinding/formsbinding.component';
+import { SumarnumerosComponent } from '../components/sumarnumeros/sumarnumeros.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
 	HooksAngular,
-	DeportesComponent
+	DeportesComponent,
+	FormsbindingComponent,
+	SumarnumerosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-	
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
