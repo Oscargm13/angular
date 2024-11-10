e5bab98b92287fa6691ed779c762300664f8354b
Oscargm13 <oskitargm5@gmail.com>
2024-11-10 14:31:34 +0100

    servicios

---
+++ serviciosangular/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComicComponent } from './components/comic/comic.component';
+import { LibreriaComponent } from './components/libreria/libreria.component';
+import { FormsModule } from '@angular/forms';
+import { ServiceComics } from './services/service.comics';
+import { provideHttpClient } from '@angular/common/http';
+import { ServicePersonas } from './services/service.personas';
+import { PersonasapiComponent } from './components/personasapi/personasapi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComicComponent,
+    LibreriaComponent, 
+    PersonasapiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [ServiceComics, ServicePersonas, provideHttpClient()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
